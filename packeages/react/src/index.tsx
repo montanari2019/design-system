c24731e746c1858f3b5e5a10f04e86e7675314a2
montanari2019 <ikaro.montanari31@gmail.com>
2023-11-07 17:04:45 -0400

    implementando novas coisas, vamos codas algo diferente

--- packeages/react/src/index.tsx
+++ packeages/react/src/index.tsx
@@ -3,4 +3,6 @@ export * from "./components/Text"
 export * from "./components/Heading"
 export * from "./components/Avatar/index"
 export * from "./components/TextInput/index"
+export * from "./components/TextArea"
+
 export * from "./components/Button"
